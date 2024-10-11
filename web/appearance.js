1a9257628c3b95e757617d1a0c2cc28f6cc13048
Makadi <rendreal@hotmail.com>
2024-10-11 23:14:31 +0300

    update appearance 2

--- web/appearance.js
+++ web/appearance.js
@@ -16,24 +16,30 @@ app.registerExtension({
                     break;
                 case "iToolsTextReplacer":
                     node.setSize([200, 58]);
+                    //node.size[0] = 200
                     node.color = LGraphCanvas.node_colors.green.color;
                     node.bgcolor = LGraphCanvas.node_colors.green.bgcolor;
                     break;
                 case "iToolsAddOverlay":
-                    node.setSize([240, 130]);
+                    //node.setSize([240, 130]);
+                    node.size[0] = 240
                     break;
                 case "iToolsLineLoader":
-                    node.setSize([200, 180]);
+                    //node.setSize([200, 180]);
+                    node.size[0] = 200
                     break;
                 case "iToolsGridFiller":
-                    node.setSize([240, 200]);
+                    //node.setSize([240, 200]);
+                    node.size[0] = 240
                     break;
                 case "iToolsLoadImagePlus":
-                    node.setSize([210, 170]);
+                    //node.setSize([210, 170]);
+                    node.size[0] = 210
                     node.color = LGraphCanvas.node_colors.pale_blue.color;
                     break;
                 case "iToolsCheckerBoard":
-                    node.setSize([270, 250]);
+                    //node.setSize([270, 250]);
+                    node.size[0] = 270
                     //node.color = LGraphCanvas.node_colors.pale_blue.color;
                     break;
             }
