59769489245604a379365753a1c1136d0c663380
Makadi <rendreal@hotmail.com>
2025-01-28 03:53:52 +0200

    paint work 2

--- web/paint_node.js
+++ web/paint_node.js
@@ -1,6 +1,106 @@
 import { app } from "../../../scripts/app.js";
 import { allow_debug } from "./js_shared.js";
 
+export class RgthreeBaseWidget {
+  constructor(name) {
+    this.last_y = 0;
+    this.mouseDowned = null;
+    this.isMouseDownedAndOver = false;
+    this.hitAreas = {};
+    this.downedHitAreasForMove = [];
+    this.name = name;
+  }
+  clickWasWithinBounds(pos, bounds) {
+    let xStart = bounds[0];
+    let xEnd = xStart + (bounds.length > 2 ? bounds[2] : bounds[1]);
+    const clickedX = pos[0] >= xStart && pos[0] <= xEnd;
+    if (bounds.length === 2) {
+      return clickedX;
+    }
+    return clickedX && pos[1] >= bounds[1] && pos[1] <= bounds[1] + bounds[3];
+  }
+  mouse(event, pos, node) {
+    var _a, _b, _c;
+    const canvas = app.canvas;
+    if (event.type == "pointerdown") {
+      this.mouseDowned = [...pos];
+      this.isMouseDownedAndOver = true;
+      this.downedHitAreasForMove.length = 0;
+      let anyHandled = false;
+      for (const part of Object.values(this.hitAreas)) {
+        if (
+          (part.onDown || part.onMove) &&
+          this.clickWasWithinBounds(pos, part.bounds)
+        ) {
+          if (part.onMove) {
+            this.downedHitAreasForMove.push(part);
+          }
+          if (part.onDown) {
+            const thisHandled = part.onDown.apply(this, [
+              event,
+              pos,
+              node,
+              part,
+            ]);
+            anyHandled = anyHandled || thisHandled == true;
+          }
+        }
+      }
+      return (_a = this.onMouseDown(event, pos, node)) !== null && _a !== void 0
+        ? _a
+        : anyHandled;
+    }
+    if (event.type == "pointerup") {
+      if (!this.mouseDowned) return true;
+      this.downedHitAreasForMove.length = 0;
+      this.cancelMouseDown();
+      let anyHandled = false;
+      for (const part of Object.values(this.hitAreas)) {
+        if (part.onUp && this.clickWasWithinBounds(pos, part.bounds)) {
+          const thisHandled = part.onUp.apply(this, [event, pos, node, part]);
+          anyHandled = anyHandled || thisHandled == true;
+        }
+      }
+      return (_b = this.onMouseUp(event, pos, node)) !== null && _b !== void 0
+        ? _b
+        : anyHandled;
+    }
+    if (event.type == "pointermove") {
+      this.isMouseDownedAndOver = !!this.mouseDowned;
+      if (
+        this.mouseDowned &&
+        (pos[0] < 15 ||
+          pos[0] > node.size[0] - 15 ||
+          pos[1] < this.last_y ||
+          pos[1] > this.last_y + LiteGraph.NODE_WIDGET_HEIGHT)
+      ) {
+        this.isMouseDownedAndOver = false;
+      }
+      for (const part of this.downedHitAreasForMove) {
+        part.onMove.apply(this, [event, pos, node, part]);
+      }
+      return (_c = this.onMouseMove(event, pos, node)) !== null && _c !== void 0
+        ? _c
+        : true;
+    }
+    return false;
+  }
+  cancelMouseDown() {
+    this.mouseDowned = null;
+    this.isMouseDownedAndOver = false;
+    this.downedHitAreasForMove.length = 0;
+  }
+  onMouseDown(event, pos, node) {
+    return;
+  }
+  onMouseUp(event, pos, node) {
+    return;
+  }
+  onMouseMove(event, pos, node) {
+    return;
+  }
+}
+
 function isLowQuality() {
   var _a;
   const canvas = app.canvas;
@@ -9,9 +109,10 @@ function isLowQuality() {
     0.5
   );
 }
-
+// extends RgthreeBaseWidget
 class DrawingApp {
   constructor() {
+    //super("iTools_draw_app");
     this.ctx = null;
     this.paint_ctx = null;
     this.node = null;
@@ -44,19 +145,22 @@ class DrawingApp {
   }
 
   startPosition() {
-    this.pos = this.node.last_mouse - this.node.last_mouse_position;
-    this.painting = true;
+    if (this.pos[1] > 80) {
+      this.painting = true;
+    }
   }
 
   endPosition() {
-    //this.ctx.beginPath();
     this.painting = false;
+    if (this.newCtx !== null) {
+      this.newCtx.beginPath();
+    }
   }
-
 
   paint() {
     if (!this.painting) return;
-    console.log("painting");
+    //console.log(this.pos)
+
     if (this.newCtx === null) {
       // Create a new canvas element or use an existing one
       const newCanvas = document.createElement("canvas");
@@ -71,6 +175,7 @@ class DrawingApp {
       this.newCanvas = newCanvas;
     }
 
+    console.log(this.node.pointer_is_down);
     // Set the drawing properties
     this.newCtx.lineWidth = this.brushSize;
     this.newCtx.lineCap = "round";
@@ -84,7 +189,6 @@ class DrawingApp {
     this.newCtx.moveTo(this.pos[0], this.pos[1]);
 
     this.ctx.drawImage(this.newCanvas, 0, 0);
-    
   }
 
   drawUI() {
@@ -98,7 +202,7 @@ class DrawingApp {
 
     // paint area
     if (!this.painting && this.newCanvas instanceof HTMLCanvasElement) {
-        this.ctx.drawImage(this.newCanvas, 0, 0);
+      this.ctx.drawImage(this.newCanvas, 0, 0);
     }
 
     // Draw brush size label
@@ -129,14 +233,16 @@ class DrawingApp {
   }
 
   draw(ctx, node, width, y, height) {
-    //const lowQuality = isLowQuality();
+    const lowQuality = isLowQuality();
+    if (lowQuality) {
+      return;
+    }
     if (this.ctx === null) {
       this.init(ctx, node, width, y, height);
     }
     this.paint();
     this.drawUI();
   }
-
 }
 
 app.registerExtension({
@@ -157,7 +263,7 @@ app.registerExtension({
     if (node.comfyClass !== "iToolsPaintNode") {
       return;
     }
-
+    node.setSize([512, 592]);
     if (allow_debug) {
       console.log("node", node);
     }
@@ -166,27 +272,30 @@ app.registerExtension({
     node.addCustomWidget(drawing_app);
 
     node.onMouseMove = (event, pos, node) => {
-      drawing_app.pos = pos; // Update the position
-      //app.graph.setDirtyCanvas(true, true);
-
-      return;
+      // Update the position after the check
+      drawing_app.pos = pos;
     };
 
     node.onMouseUp = (event, pos, node) => {
-      node.allow_dragnodes = true;
+      console.log("Mouse_up");
       drawing_app.endPosition();
+      node.allow_dragnodes = true;
       return;
     };
 
     node.onMouseDown = (event, pos, node) => {
-      if (pos[1] > 30) {
-        node.allow_dragnodes = false;
-        //   node.allow_interaction = false;
-      }
-      //node.ctx.restore()
       console.log("node", node);
       console.log("app", app);
-      drawing_app.startPosition();
+      if (pos[1] > 30) {
+        node.allow_dragnodes = false;
+      }
+      if(drawing_app.painting) {
+        drawing_app.endPosition();}
+      else{
+        drawing_app.startPosition();
+      }
+      
+
       return;
     };
   },
